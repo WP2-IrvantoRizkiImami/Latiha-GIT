2898a36f485935d2fb641c4d050f74b257321168
WP2-IrvantoRizkiImami <91044802+WP2-IrvantoRizkiImami@users.noreply.github.com>
2021-09-20 12:57:17 +0700

    Add files via upload

---
+++ contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controllers
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>"
+        echo "Nama saya adalah Irvanto Rizki Imami
+        	Saya tinggal di daerah Ciluer
+        	Olahraga yang saya sukai adalah
+        	Bulutangkis";
+    } 
+}
